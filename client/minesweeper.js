83af221f46575da1cfc20463899cadd13c0ad649
JohnB <john.baylor@gmail.com>
2014-09-13 00:06:36 -0700

    Rremoved duplicate templates

--- client/minesweeper.js
+++ client/minesweeper.js
@@ -6,20 +6,18 @@
 //     mines: 0.05
 // });
 
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+// counter starts at 0
+Session.setDefault("counter", 0);
 
-  Template.controls.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+Template.controls.helpers({
+  counter: function () {
+    return Session.get("counter");
+  }
+});
 
-  Template.controls.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-}
+Template.controls.events({
+  'click button': function () {
+    // increment the counter when button is clicked
+    Session.set("counter", Session.get("counter") + 1);
+  }
+});
