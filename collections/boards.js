b32ff4f8578ac6f7f2e701798b11c9f4c14acc61
JohnB <john.baylor@gmail.com>
2014-09-17 21:38:13 -0700

    Internal board initializes correctly.

--- collections/boards.js
+++ collections/boards.js
@@ -1,12 +1,17 @@
 var defaults = {
-  width: 10,
-  height: 10,
-  numBombs: 20,
+  width: 3,
+  height: 3,
+  numBombs: 2,
   _offBoardWidth: 1,
   _cells: []
 }
 
 Boards = new Meteor.Collection("boards", {
+  // Inspiration for the transform function came from
+  // http://stackoverflow.com/questions/15007231/whats-the-best-way-to-attach-behavior-to-a-meteor-collection
+  //
+  // TODO: improve http://docs.meteor.com/#mongo_collection so the
+  //       transform-as-constructor model is more clear.
   transform: function(options) {
     var obj = _.defaults(options, defaults);
     obj.fullWidth = obj.width + 2 * obj._offBoardWidth;
@@ -22,32 +27,39 @@ Boards = new Meteor.Collection("boards", {
     // This allows us to reference our 8 neighbors with integer offsets.
     obj.neighbors = [
         -(obj.fullWidth+1), -obj.fullWidth, -(obj.fullWidth-1),
-        1,                                  1,
-        obj.fullWidth+1,     obj.fullWidth, obj.fullWidth-1
+        -1,                                  1,
+        obj.fullWidth-1,     obj.fullWidth, obj.fullWidth+1
       ];
     obj.eachNeighbor = function(idx, fn) {
-      var dispatch = function(element, index, array) {
+      obj.neighbors.forEach( function(delta, ignoreInt, ignoreArray) {
+        var index = idx + delta;
         if ( !obj.is_offBoard(index) ) {
-          fn(element);
+          fn( obj.cells()[index] );
         }
-      }
-      obj.neighbors.forEach( dispatch )
+      })
+    };
+    obj.plantBomb = function(idx) {
+      var increaseNearbyBombs = function(element) {
+        element.nearbyBombs += 1;
+      };
+      var cell = obj.cells()[idx]
+      cell.cellType = 'bomb';
+      obj.eachNeighbor(idx, increaseNearbyBombs);
     };
     obj.addBombs = function() {
-      var plantBomb = function(idx) {
-        var increaseNearbyBombs = function(element) {
-          element.nearbyBombs += 1;
-        }
-        obj.cells()[idx].cellType = 'bomb';
-        obj.eachNeighbor(idx, increaseNearbyBombs);
-      }
       var bombs = obj.numBombs;
       while( bombs > 0 ) {
         var idx = parseInt( Math.random() * obj.numCells );
         console.log(idx);
-        if( obj.isEmpty(idx) ) {
-          plantBomb(idx);
+        if( obj.is_empty(idx) ) {
+          obj.plantBomb(idx);
+          bombs -= 1;
         }
+      }
+    };
+    obj.rows = function(perRowFn) {
+      for( i = fullWidth + _offBoardWidth; i < bottomEdge; i += fullWidth ) {
+        perRowFn( _cells().slice(i, i+width) );
       }
     };
     obj.topEdge    = obj.fullWidth;
@@ -56,21 +68,24 @@ Boards = new Meteor.Collection("boards", {
     obj.rightEdge  = obj.width + obj._offBoardWidth;
 
     obj.is_off_top_edge    = function(idx) { return idx < obj.topEdge };
-    obj.is_off_bottom_edge = function(idx) { return idx > obj.bottomEdge };
+    obj.is_off_bottom_edge = function(idx) { return idx >= obj.bottomEdge };
     obj.is_off_left_edge   = function(idx) { return (idx % obj.fullWidth) < obj.leftEdge };
-    obj.is_off_right_edge  = function(idx) { return (idx % obj.fullWidth) >= obj.rigtEdge };
+    obj.is_off_right_edge  = function(idx) { return (idx % obj.fullWidth) >= obj.rightEdge };
     obj.is_off_board = function(idx) {
       return obj.is_off_top_edge(idx)  || obj.is_off_bottom_edge(idx) ||
              obj.is_off_left_edge(idx) || obj.is_off_right_edge(idx);
     }
+    console.log(obj.inspect);
 
     obj.cells = function() {
       if (obj._cells.length < obj.numCells) {
         obj._cells = new Array(obj.numCells);
         for (i = 0; i < obj.numCells; i++) {
+          var cellType = obj.is_off_board(i) ? 'offBoard' : 'empty';
+          console.log(''+i+': '+cellType);
           obj._cells[i] = {
             index: i,
-            cellType: obj.is_off_board(i) ? 'offBoard' : 'empty',
+            cellType: cellType,
             nearbyBombs: 0
           };
         }
@@ -81,40 +96,5 @@ Boards = new Meteor.Collection("boards", {
 
     // finish the "constructor" by returning the object
     return obj;
-  }
-});
-
-Meteor.methods({
-  boardCreate: function(attributes) {
-    var offBoardWidth = 1,
-        width =  attributes['width'] || 10,
-        height = attributes['height'] || 10;
-    var fullWidth = width + 2 * offBoardWidth,
-        fullHeight = height + 2 * offBoardWidth,
-        cellTypes = ['offBoard', 'empty', 'bomb'];
-    // Represent the board as a one-dimensional array of cells,
-    // with "offBoard" cells around the edges so we don't have to
-    // continually check whether we're off the board or not.
-    // This allows us to reference our 8 neighbors with integer offsets.
-    var neighbors = [-(fullWidth+1), -fullWidth, -(fullWidth-1),
-                     -1,                         1,
-                     fullWidth+1,     fullWidth,   fullWidth-1]
-    var numCells = fullWidth * fullHeight;
-    var cells = new Array(numCells);
-    for (i = 0; i < numCells; i++) {
-      // This is the only spot where we should have to calculate offBoard-edness
-      var cellType = 'empty';
-      if (i < fullWidth * offBoardWidth)              { cellType = 'offBoard'; };
-      if ((i       % fullWidth) < offBoardWidth)      { cellType = 'offBoard'; };
-      if (((i + 1) % fullWidth) < offBoardWidth)      { cellType = 'offBoard'; };
-      if (i >= numCells - offBoardWidth * fullWidth)  { cellType = 'offBoard'; };
-
-      cells[i] = {cellType: cellType, nearbyBombs: 0};
-    }
-    // TODO: randomly add bombs
-
-    return {
-      cells: cells
-    };
   }
 });
