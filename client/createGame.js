fc65745cb3827fbd02ee7d658d19c7b193eabee4
JohnB <john.baylor@gmail.com>
2014-10-04 11:46:59 -0700

    Finished game creation/insertion

--- client/createGame.js
+++ client/createGame.js
@@ -6,15 +6,32 @@ Template.createGame.helpers({
 Template.createGame.events({
     'click button.create': function() {
         Session.set('creatingGame', 1 );
-        alert('oh crap it worked');
     },
     'click button.play': function() {
-        var prefix = $('button [name=gameNamePrefix]').val();
-        var fullName = prefix + '_' + Date.now();
+        var now = new Date();
+        // TODO: figure out where to put utility functions like this.
+        var yyyymmdd_hhmmss = function(dateIn) {
+            var yyyy= dateIn.getFullYear();
+            var mm  = dateIn.getMonth()+1; // getMonth() is zero-based
+            var dd  = dateIn.getDate();
+            var hh  = dateIn.getHours();
+            var min = dateIn.getMinutes();
+            var ss  = dateIn.getSeconds();
+            return '' + (10000*yyyy + 100*mm + dd) + '_' + (10000*hh + 100*min + ss);
+        };
+        var prefix = $('input[name=gameNamePrefix]').val();
+        var fullName = prefix + '_' + yyyymmdd_hhmmss(now);
         Session.set('gameNamePrefix', prefix );
         Session.set('creatingGame', undefined );
-        Session.set('currentGame', fullName );
 
-        alert('TODO: Games.insert("'+fullName+'")');
+        Games.insert( {created_at: now, name: fullName, user: Meteor.userId()}, function(err,id) {
+            if (err) {
+                Session.set('currentGame', undefined );
+                console.log(err);
+                alert('oops, there was an error: ' + err);
+            } else {
+                Session.set('currentGame', id );
+            }
+        } );
     }
 });
