73f8caae45857bee37b72ed1ee46d4619186f6ba
JohnB <john.baylor@gmail.com>
2014-10-08 14:42:29 -0700

    Display basic board, with big hints of where the bombs are. TODO: hide bombs and add interaction.

---
+++ client/showGame.js
@@ -0,0 +1,18 @@
+Template.showGame.helpers({
+    boardRow: function(game) {
+        if (!game) { return []; }  // why is it undefined the first time thru???
+        console.log(game.bombs);
+
+        // There must be a better way to create this board-size array
+        var result = [];
+        for(i = 0; i < game.height; i++) {
+            var row = [];
+            for(j = 0; j < game.width; j++) {
+                var cell = i*game.width+j;
+                row.push({cell: cell, bomb: (game.bombs.indexOf(cell) >= 0) ? 'x' : '' });
+            }
+            result.push({row: row});
+        }
+        return result;
+    }
+});
